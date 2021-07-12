24293c27fc439e995805ec82550bfbd32cacb37f
Andrexus23 <andrewsamsunguser@gmail.com>
2021-07-12 12:02:32 +0300

    done

---
+++ 3rd_iter/final/src/Edge.java
@@ -0,0 +1,73 @@
+
+class Edge {
+    private Graph.LeftVertex leftV;
+    private Graph.RightVertex rightV;
+    private boolean includeMatching; // флаг входит ли ребро в паросочетание
+    private boolean isResearchingNow;
+
+    public Edge(Graph.LeftVertex leftV, Graph.RightVertex rightV) {
+        this.leftV = leftV;
+        this.rightV = rightV;
+        this.includeMatching = false;
+        this.isResearchingNow = false;
+    }
+
+    public Edge(Edge toBeCopied, Graph.LeftVertex leftV, Graph.RightVertex rightV) {
+        this.leftV = leftV;
+        this.rightV = rightV;
+        this.includeMatching = toBeCopied.includeMatching;
+        this.isResearchingNow = toBeCopied.isResearchingNow;
+    }
+
+    public Graph.Vertex go(Graph.Vertex preV, boolean isFurther) {
+        if(isFurther) this.setResearchingNow(); //  помечаем текущее ребро как просматриваемое сейчас
+        else this.unsetResearchingNow();
+        preV.unsetResearchingNow(); // начальную вершину текущего ребра отмечаем как непросматриваемую сейчас
+        Graph.Vertex result = preV.getNeedV(leftV, rightV); // получаем конечную вершину текущего ребра
+        result.setResearchingNow(); // конечную вершину текущего ребра отмечаем как просматриваемую сейчас
+        return result;
+    }
+
+    public boolean isIncludeMatching() {
+        return includeMatching;
+    }
+
+    public void doIncludeInMatching() {
+        includeMatching = true;
+        leftV.setMatchingEdge(this);
+        rightV.setMatchingEdge(this);
+    }
+
+
+    public void doExcludeFromMatching() {
+        includeMatching = false;
+        leftV.setMatchingEdge(null);
+        rightV.setMatchingEdge(null);
+    }
+    /*@Override
+    public boolean equals(Object obj) {
+       if(this == obj)  return true;
+       if(obj instanceof Edge){
+           Edge other = (Edge) obj;
+           return (other.leftV.getName().equals(this.leftV.getName()) && other.rightV.getName().equals(this.rightV.getName()));
+       }
+       return false;
+    }*/
+
+    public String toString() {
+        return leftV.getName() + "--" + rightV.getName();
+    }
+    public void setResearchingNow(){
+        this.isResearchingNow = true;
+    }
+    public void unsetResearchingNow(){
+        this.isResearchingNow =false;
+    }
+    public boolean getIsResearchingNow(){return isResearchingNow;};
+    public Graph.LeftVertex getLeftVertex(){
+        return this.leftV;
+    }
+    public Graph.RightVertex getRightVertex(){
+        return this.rightV;
+    }
+}
---
+++ 3rd_iter/final/src/MyLogger.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+public class MyLogger {
+    private StringBuilder logMes;
+    private Date date;
+    MyLogger(){
+        logMes = new StringBuilder();
+        date = new Date();
+    }
+    public void writeLog(String str){
+        logMes.append(new Date().toString().substring(11,19)).append(" ").append(str);
+    }
+
+    public StringBuilder getLogMes(){
+      return logMes;
+    }
+
+    public void setLogMes(StringBuilder logMes) {this.logMes = logMes;}
+}
